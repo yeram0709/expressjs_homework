aac857d6647b0b274a23cbb74390250b188dad5e
yeram0709 <yeram0709.kim@gmail.com>
2024-02-01 20:10:15 +0900

    second

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.set("port", 3000);
 
 //export한 router 가져오기
-const indexRouter = require("./router/index");
+const indexRouter = require("./router");
 const testRouter = require("./router/test");
 
 //router 연결
 app.use("/", indexRouter);
 
+//test 연결
+app.use("/test",testRouter);
 
   app.listen(app.get("port"), () => {
     console.log(`Server On Port ${app.get("port")}`);
