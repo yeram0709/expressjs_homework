bdb26fb286d0609e61ca589f74cf090aec30db74
yeram0709 <yeram0709.kim@gmail.com>
2024-02-04 16:02:32 +0900

    up

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
+const { sequelize } = require("./models");
 
 const app = express();
 
 app.set("port", 3000);
 
-//export한 router 가져오기
-const indexRouter = require("./router");
-const testRouter = require("./router/test");
-
-//router 연결
-app.use("/", indexRouter);
-
-//test 연결
-app.use("/test",testRouter);
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("DB 연결 성공");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
   app.listen(app.get("port"), () => {
     console.log(`Server On Port ${app.get("port")}`);
